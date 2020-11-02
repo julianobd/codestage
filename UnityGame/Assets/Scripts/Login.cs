fc27e639cdfc60a52aac91841d45deafcb06dc6d
julianobd <julianobd@yahoo.com.br>
2020-11-01 22:45:44 -0300

    upload dos projetos

---
+++ UnityGame/Assets/Scripts/Login.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public InputField txtLogin;
+    public InputField txtSenha;
+    public GameObject lblError;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("play");
+    }
+
+    public void Logar()
+    {
+        if (txtLogin.text == "demo" && txtSenha.text == "demo")
+        {
+            lblError.SetActive(false);
+            SceneManager.LoadScene("main");
+        } else
+        {
+            lblError.SetActive(true);
+            Debug.Log("erro");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityGame/Assets/Scripts/PlayGame.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour
+{
+    public AudioClip musica1;
+    public AudioClip musica2;
+    public AudioClip musica3;
+    public AudioClip musica4;
+
+    public AudioSource audioSource;
+
+    public GameObject body1;
+    public GameObject body2;
+    public GameObject body21;
+    public GameObject body3;
+
+    public Image imgProgress;
+    float tempo;
+
+    public List<GameObject> arrGameObjs;
+
+
+    public List<GameObject> arrGameObjs2;
+
+    private bool isCoroutineExecuting = false;
+    private bool isPlaying = false;
+
+    public void Answer1()
+    {
+        if (isCoroutineExecuting)
+            return;
+        arrGameObjs.ForEach(x => x.SetActive(false));
+        arrGameObjs.OrderBy(x => Guid.NewGuid()).FirstOrDefault().SetActive(true);
+        StartCoroutine(ExecuteAfterTime(3f, () =>
+        {
+            body2.SetActive(false);
+            body21.SetActive(true);
+        }));
+    }
+
+    public void Answer2()
+    {
+        if (isCoroutineExecuting)
+            return;
+        arrGameObjs2.ForEach(x => x.SetActive(false));
+        arrGameObjs2.OrderBy(x => Guid.NewGuid()).FirstOrDefault().SetActive(true);
+        StartCoroutine(ExecuteAfterTime(3f, () =>
+        {
+            body21.SetActive(false);
+            body3.SetActive(true);
+        }));
+    }
+
+    IEnumerator ExecuteAfterTime(float time, Action task)
+    {
+        if (isCoroutineExecuting)
+            yield break;
+        isCoroutineExecuting = true;
+        yield return new WaitForSeconds(time);
+        task();
+        isCoroutineExecuting = false;
+    }
+
+
+    public void PlaySound()
+    {
+        imgProgress.fillAmount = 0;
+        tempo = 0;
+        audioSource.clip = musica1;
+        audioSource.Play();
+        isPlaying = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        if (tempo > 30)
+        {
+            if (isPlaying)
+            {
+                audioSource.Stop();
+                isPlaying = false;
+                body1.SetActive(false);
+                body2.SetActive(true);
+            }
+        }
+        else
+        {
+            if (isPlaying)
+            {
+                imgProgress.fillAmount = (tempo / 30f);
+            }
+        }
+    }
+}
